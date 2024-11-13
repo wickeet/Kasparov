72c8502332127364393666e7f3aca38f07be0808
wickeet <karimsaidl@correo.ugr.es>
2024-11-13 12:22:25 +0100

    Archivo con las variables constantes (tambien se me ha olvidado)

---
+++ kasparov/const.py
@@ -0,0 +1,14 @@
+#Tamaño de la ventana
+WIDTH = 800
+HEIGHT = 800
+
+#Tamaño del tablero
+ROWS = 8
+COLS = 8
+QUAD_SIZE = WIDTH // COLS
+
+#Colores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_GREEN = (234, 235, 200)
+DARK_GREEN = (119, 154, 88)
