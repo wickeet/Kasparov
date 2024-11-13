a86050fc551d5181944984af89c54cc42dcac2e2
wickeet <karimsaidl@correo.ugr.es>
2024-11-13 12:11:47 +0100

    Clase Quad creada (hace referencia a una casilla del tablero)

---
+++ kasparov/quad.py
@@ -0,0 +1,9 @@
+class Quad:
+
+    def __init__(self, row, col, piece=None):
+        self.row = row
+        self.col = col
+        self.piece = piece
+    
+    def has_piece(self):
+        return self.piece != None
